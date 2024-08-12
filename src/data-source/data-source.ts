11558d2217de04b5ded39f5cc5e103cf37699864
Christian <christian@redbonzai.com>
2024-08-12 00:26:28 -0500

    feat: implemented migrations into the api

---
+++ src/data-source/data-source.ts
@@ -0,0 +1,27 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import { Game, GameCell } from '../entities';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: '0.0.0.0',
+  port: 5432,
+  username: 'local',
+  password: 'local',
+  database: 'local',
+  entities: [Game, GameCell],
+  // eslint-disable-next-line no-undef
+  migrations: [join(__dirname + '/../migrations/*{.ts,.js}')],
+  synchronize: false,
+});
+
+// Call initialize only if necessary, usually within your app's entry point.
+AppDataSource.initialize()
+  .then(() => {
+    // eslint-disable-next-line no-undef
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-undef
+    console.error('Error during Data Source initialization', err);
+  });
